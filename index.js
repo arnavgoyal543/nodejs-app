d1637f4df221c70c473026a678f7661e242800bc
arnavgoyal543 <goyaldev2003@gmail.com>
2024-12-26 20:54:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const PORT = 9090;
+const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
